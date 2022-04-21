25d312298f8438cd55c623d0594612bcaa161b81
Max Grabner <max.grabner@staedter.io>
2022-04-21 22:00:52 +0100

    fixed mod

--- contracts/margined_engine/src/testing/mod.rs
+++ contracts/margined_engine/src/testing/mod.rs
@@ -2,6 +2,7 @@
 mod cw_token_add_remove_margin_tests;
 mod cw_token_liquidation_frontrun_hack_tests;
 mod cw_token_liquidation_tests;
+mod cw_token_pay_funding_tests;
 mod cw_token_position_fee_tests;
 mod fee_calculation_tests;
 mod fluctuation_tests;
@@ -11,7 +12,6 @@ mod native_token_add_remove_margin_tests;
 mod native_token_position_fee_tests;
 mod open_notional_tests;
 mod pausable_tests;
-mod pay_funding_tests;
 mod personal_position_tests;
 mod position_liquidation_tests;
 mod position_tests;
