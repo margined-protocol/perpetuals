dc79702c58b18b318a99133f033de73110c45286
Max Grabner <max.grabner@staedter.io>
2022-04-23 10:55:51 +0100

    first native liquidation test passed

--- contracts/margined_engine/src/testing/mod.rs
+++ contracts/margined_engine/src/testing/mod.rs
@@ -9,6 +9,7 @@ mod fluctuation_tests;
 mod margin_engine_tests;
 mod margin_ratio_tests;
 mod native_token_add_remove_margin_tests;
+mod native_token_liquidation_tests;
 mod native_token_position_fee_tests;
 mod open_notional_tests;
 mod pausable_tests;
