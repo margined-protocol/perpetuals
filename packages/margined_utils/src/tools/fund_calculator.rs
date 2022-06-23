14c37e3a3c008176c2693bc9b16b346e76e994ce
Max Grabner <max.grabner@staedter.io>
2022-06-23 13:44:14 +0100

    made simpler

--- packages/margined_utils/src/tools/fund_calculator.rs
+++ packages/margined_utils/src/tools/fund_calculator.rs
@@ -21,7 +21,6 @@ use margined_perp::margined_vamm::{CalcFeeResponse, Direction, QueryMsg as VammQ
 
 pub const SIX_D_P: Uint128 = Uint128::new(1_000_000u128); // this is 6d.p.
 
-// this function needs to be used before a (new) position is opened
 pub fn calculate_funds_needed<Q: Querier>(
     querier: &Q,
     engine: Addr,
@@ -39,7 +38,7 @@ pub fn calculate_funds_needed<Q: Querier>(
     let fee_amount: Uint128 = query_vamm_fees(querier, vamm.to_string(), new_notional)?;
 
     // check if they have an existing position so we can calculate if someone owes margin
-    let mut position: Position = query_existing_position(
+    let position: Position = query_existing_position(
         querier,
         engine.to_string(),
         vamm.to_string(),
@@ -47,56 +46,33 @@ pub fn calculate_funds_needed<Q: Querier>(
     )?;
 
     // Check the unrealised PnL and add it to: (existing position + margin)
-    let unrealised_pnl = query_existing_position_pnl(
+    let unrealized_pnl = query_existing_position_pnl(
         querier,
         engine.to_string(),
         vamm.to_string(),
         trader.to_string(),
     )?;
 
-    // Check if unrealised_pnl is positive or negative
-    // if it is negative, we check that it is smaller than position.margin to prevent overflow
-    let mut margin_owed: Integer = Integer::zero();
-    match unrealised_pnl.unrealized_pnl.is_negative() {
-        true => {
-            position.margin = if position.margin > unrealised_pnl.unrealized_pnl.value {
-                position.margin - unrealised_pnl.unrealized_pnl.value
-            } else {
-                margin_owed =
-                    Integer::new_positive(unrealised_pnl.unrealized_pnl.value - position.margin);
-                Uint128::zero()
-            }
-        }
-        false => position.margin += unrealised_pnl.unrealized_pnl.value,
-    };
-
     // First we check if they are increasing the position or not
     let is_increase: bool = position.direction == Direction::AddToAmm && side == Side::Buy
         || position.direction == Direction::RemoveFromAmm && side == Side::Sell;
 
-    // If we are increasing, then we simply add the margin for the latest trade (quote_asset_amount)
-    // If we are not increasing, then we need to calculate whether the position is reversing or not
-    // If the position doesnt reverse, we don't need any more margin (we just decrease leverage + notional)
-    // If the position does reverse, then we need to calculate the new notional position and
-    // divide it by the leverage to find the margin required (first summand)
-    match is_increase {
-        true => {
-            margin_owed += Integer::new_positive(quote_asset_amount);
-        }
+    let margin_owed: Integer = match is_increase {
+        true => Integer::new_positive(quote_asset_amount),
         false => {
-            if unrealised_pnl.position_notional < new_notional {
-                margin_owed += Integer::new_positive(
-                    (new_notional - unrealised_pnl.position_notional)
+            if new_notional > unrealized_pnl.position_notional {
+                Integer::new_positive(
+                    (new_notional - unrealized_pnl.position_notional)
                         .checked_mul(SIX_D_P)?
                         .checked_div(leverage)?,
-                ) - Integer::new_positive(position.margin);
+                ) - Integer::new_positive(position.margin)
+                    - unrealized_pnl.unrealized_pnl
+            } else {
+                Integer::zero()
             }
         }
     };
 
-    // calculate the final funds owed.
-    // if margin_owed is negative, then the trader only pays fee_amount.
-    // if margin_owed is positive then the trader pays fee_amount + margin_owed
     let funds_owed = if margin_owed.is_positive() {
         fee_amount + margin_owed.value
     } else {
